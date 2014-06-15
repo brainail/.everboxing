351604df17c87e3e5c3e75260dfbc27fcc57fd47
Malyshev Yegor <brainail@yandex-team.ru>
2014-06-15 21:15:22 +0300

    Fixed the package name. New assets. And other minor init changes

---
+++ EverboxingModule/src/main/java/org/brainail/everboxing/windows/activities/NavDrawerActivity.java
@@ -0,0 +1,91 @@
+package org.brainail.everboxing.windows.activities;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.brainail.everboxing.R;
+import org.brainail.everboxing.windows.fragments.NavDrawerFragment;
+import org.brainail.everboxing.windows.fragments.PlaceholderFragment;
+
+public class NavDrawerActivity extends ActionBarActivity implements NavDrawerFragment.NavigationDrawerCallbacks {
+
+    private NavDrawerFragment mNavDrawerFragment;
+
+    // Used to store the last screen title. For use in {@link #restoreActionBar()}.
+    private CharSequence mLastScreenTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_root);
+        initializeComponents();
+    }
+
+    public void initializeComponents() {
+        mLastScreenTitle = getTitle();
+        mNavDrawerFragment = (NavDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mNavDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout));
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(final int position) {
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        final Fragment placeholderFragment = PlaceholderFragment.newInstance(position + 1);
+        fragmentManager.beginTransaction().replace(R.id.container, placeholderFragment).commit();
+    }
+
+    public void onSectionAttached(int number) {
+        switch (number) {
+
+            case 1:
+                mLastScreenTitle = getString(R.string.title_section1);
+                break;
+
+            case 2:
+                mLastScreenTitle = getString(R.string.title_section2);
+                break;
+
+            case 3:
+                mLastScreenTitle = getString(R.string.title_section3);
+                break;
+
+        }
+    }
+
+    public void restoreActionBar() {
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mLastScreenTitle);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!mNavDrawerFragment.isDrawerOpen()) {
+            // Only show items in the action bar relevant to this screen
+            // if the drawer is not showing. Otherwise, let the drawer
+            // decide what to show in the action bar.
+            getMenuInflater().inflate(R.menu.main_ever_boxing, menu);
+            restoreActionBar();
+            return true;
+        }
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (R.id.action_settings == item.getItemId()) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ EverboxingModule/src/main/java/org/brainail/everboxing/windows/fragments/PlaceholderFragment.java
@@ -0,0 +1,43 @@
+package org.brainail.everboxing.windows.fragments;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.brainail.everboxing.windows.activities.NavDrawerActivity;
+import org.brainail.everboxing.R;
+
+public class PlaceholderFragment extends Fragment {
+
+    // The fragment argument representing the section number for this fragment.
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public static PlaceholderFragment newInstance(int sectionNumber) {
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public PlaceholderFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_test, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((NavDrawerActivity) activity).onSectionAttached(getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+
+}
