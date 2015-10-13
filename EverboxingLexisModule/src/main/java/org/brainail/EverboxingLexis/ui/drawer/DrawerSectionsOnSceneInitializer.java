af084f9189957003afa7f4136c66c83d4f97bab1
Malyshev Yegor <wsemirz@gmail.com>
2015-10-13 12:02:20 +0300

    Fix drawer for lexis

--- EverboxingLexisModule/src/main/java/org/brainail/EverboxingLexis/ui/drawer/DrawerSectionsOnSceneInitializer.java
+++ EverboxingLexisModule/src/main/java/org/brainail/EverboxingLexis/ui/drawer/DrawerSectionsOnSceneInitializer.java
@@ -2,9 +2,11 @@ package org.brainail.EverboxingLexis.ui.drawer;
 
 import com.malinskiy.materialicons.Iconify;
 
+import org.brainail.EverboxingLexis.R;
 import org.brainail.EverboxingLexis.ui.activities.SectionedDrawerActivity;
 import org.brainail.EverboxingLexis.ui.activities.SettingsActivity;
 import org.brainail.EverboxingLexis.ui.views.BaseIcon;
+import org.brainail.EverboxingLexis.utils.tool.ToolResources;
 
 import itkach.aard2.ui.fragments.LexisBookmarksFragment;
 import itkach.aard2.ui.fragments.LexisDictionariesFragment;
@@ -50,46 +52,51 @@ public final class DrawerSectionsOnSceneInitializer {
             // Lookup
             scene.addDrawerSection (
                     new DrawerSection (scene, DrawerSection.LayoutType.NORMAL)
-                            .withTitle ("Lookup")
-                            .withName ("Lookup")
-                            .withIcon (BaseIcon.icon (scene, Iconify.IconValue.zmdi_search))
+                            .withTitle (ToolResources.string (R.string.section_lookup))
+                            .withName (ToolResources.string (R.string.section_lookup))
+                            .withIcon (BaseIcon.defIcon(scene, Iconify.IconValue.zmdi_search))
                             .withTarget (new LexisLookupFragment ())
+                            .withSectionColor(ToolResources.retrievePrimaryColor (scene))
             );
 
             // Bookmarks
             scene.addDrawerSection (
                     new DrawerSection (scene, DrawerSection.LayoutType.NORMAL)
-                            .withTitle ("Bookmarks")
-                            .withName ("Bookmarks")
-                            .withIcon (BaseIcon.icon (scene, Iconify.IconValue.zmdi_bookmark_outline))
+                            .withTitle (ToolResources.string (R.string.section_bookmarks))
+                            .withName (ToolResources.string (R.string.section_bookmarks))
+                            .withIcon (BaseIcon.defIcon(scene, Iconify.IconValue.zmdi_bookmark_outline))
                             .withTarget (new LexisBookmarksFragment ())
+                            .withSectionColor(ToolResources.retrievePrimaryColor (scene))
             );
 
             // History
             scene.addDrawerSection (
                     new DrawerSection (scene, DrawerSection.LayoutType.NORMAL)
-                            .withTitle ("History")
-                            .withName ("History")
-                            .withIcon (BaseIcon.icon (scene, Iconify.IconValue.zmdi_time_restore))
+                            .withTitle (ToolResources.string (R.string.section_history))
+                            .withName (ToolResources.string (R.string.section_history))
+                            .withIcon (BaseIcon.defIcon(scene, Iconify.IconValue.zmdi_time_restore))
                             .withTarget (new LexisHistoryFragment ())
+                            .withSectionColor(ToolResources.retrievePrimaryColor (scene))
             );
 
             // Dictionaries
             scene.addDrawerSection (
                     new DrawerSection (scene, DrawerSection.LayoutType.NORMAL)
-                            .withTitle ("Dictionaries")
-                            .withName ("Dictionaries")
-                            .withIcon (BaseIcon.icon (scene, Iconify.IconValue.zmdi_collection_text))
+                            .withTitle (ToolResources.string (R.string.section_dictionaries))
+                            .withName (ToolResources.string (R.string.section_dictionaries))
+                            .withIcon (BaseIcon.defIcon(scene, Iconify.IconValue.zmdi_collection_text))
                             .withTarget (new LexisDictionariesFragment ())
+                            .withSectionColor(ToolResources.retrievePrimaryColor (scene))
             );
 
             // Settings
             scene.addDrawerSection (
                     new DrawerSection (scene, DrawerSection.LayoutType.NORMAL)
-                            .withName ("Settings")
+                            .withName (ToolResources.string (R.string.settings_activity_title))
                             .withLocationType (DrawerSection.LocationType.HELP)
-                            .withIcon (BaseIcon.icon (scene, Iconify.IconValue.zmdi_settings))
+                            .withIcon (BaseIcon.icon(scene, Iconify.IconValue.zmdi_settings))
                             .withTarget (SettingsActivity.class)
+                            // .withSectionColor(ToolResources.retrievePrimaryColor (scene))
             );
         }
 
